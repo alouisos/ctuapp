8cf7b2b26a5a032dc4c556300d3f5908da703bf1
LOUIZOS-ALEXANDROS LOUIZOS <alouisos@gmail.com>
2014-07-09 20:43:03 +0100

    minor corrections

--- app/routes.js
+++ app/routes.js
@@ -143,7 +143,7 @@ app.get('/logout-earnest', function(req, res) {
 
 		// process the signup form
 		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/cctu-clinical-trials', // redirect to the secure profile section
+			successRedirect : '/', // redirect to the secure profile section
 			failureRedirect : '/signup', // redirect back to the signup page if there is an error
 			failureFlash : true // allow flash messages
 		}));
